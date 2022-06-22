86b17b7ccdec69b9649548a0060db38b68df09f9
Nigraum <yan.corintima@hotmail.com>
2022-06-22 09:36:17 -0300

    Finished sidebar

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -3,4 +3,4 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 export const client = new ApolloClient({
   uri: 'https://api-sa-east-1.graphcms.com/v2/cl4pdipyx1rf301xkfhdmhy4k/master',
   cache: new InMemoryCache()
-})
+});
