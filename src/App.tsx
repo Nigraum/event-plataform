3d65ca02de55bdbe9ba2e9696d71e00b6d3266eb
Nigraum <yan.corintima@hotmail.com>
2022-06-23 11:54:08 -0300

    Changing apolloprovider to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { ApolloProvider } from "@apollo/client"
+import { client } from "./lib/apollo"
 import { Router } from "react-router-dom"
 import { Event } from "./pages/Event"
 
 function App() {
   return (
+      <ApolloProvider client={client}>
       <Router />
+      </ApolloProvider>
   )
 }
 
